2022b35f61d4ccc8bd9e123c00237a1c125dff18
san4es <san4es113@gmail.com>
2017-05-29 09:33:47 +0300

    s

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -13,30 +13,49 @@ class MakesController < ApplicationController
   end
 
   def report
-    uri = URI.parse ENV['CLOUDMQTT_URL'] || 'mqtt://localhost:1883'
-    conn_opts = {
-      remote_host: uri.host,
-      remote_port: uri.port,
-      username: uri.user,
-      password: uri.password,
-    }
+        task :mqtt_publish do
+      # Create a hash with the connection parameters from the URL
+      uri = URI.parse ENV['CLOUDMQTT_URL'] || '{MQTT URL設定}'
+      conn_opts = {
+        remote_host: uri.host,
+        remote_port: uri.port,
+        username: uri.user,
+        password: uri.password,
+      }
 
-    Thread.new do
       MQTT::Client.connect(conn_opts) do |c|
-        # The block will be called when you messages arrive to the topic
-        c.get('test') do |topic, message|
-          puts "#{topic}: #{message}"
-        end
-      end
-    end
-
-    MQTT::Client.connect(conn_opts) do |c|
-      # publish a message to the topic 'test'
-      loop do
+        # publish a message to the topic 'test'
         c.publish('test', 'Hello World')
         sleep 1
       end
     end
+
+    task :mqtt_subscribe do
+
+      # Create a hash with the connection parameters from the URL
+      uri = URI.parse ENV['CLOUDMQTT_URL'] || '{MQTT URL設定}'
+      conn_opts = {
+        remote_host: uri.host,
+        remote_port: uri.port,
+        username: uri.user,
+        password: uri.password,
+      }
+
+      MQTT::Client.connect(conn_opts) do |c|
+        # The block will be called when you messages arrive to the topic
+        loop do
+          c.get('test') do |topic, message|
+            puts "#{topic}: #{message}"
+          end
+        end
+      end
+
+end
+
+
+
+
+
   end
 
 

9fe06356d4215c496f59d955e7d1af2d3498a3c3
san4es <san4es113@gmail.com>
2017-05-29 09:02:47 +0300

    d

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -29,7 +29,7 @@ MQTT::Client.connect(conn_opts) do |c|
   # publish a message to the topic 'test'
   loop do
     c.publish('test', 'Hello World')
-    @message="publicate"
+    render text:"request to generate a report added to the queue"
     sleep 10
   end
 end
@@ -39,8 +39,7 @@ end
         # The block will be called when you messages arrive to the topic
         c.get('test') do |topic, message|
           puts "#{topic}: #{message}"
-          @message=kek
-          @message=message
+          render text:"request to generate a report added to the queue"
         end
       end
     end

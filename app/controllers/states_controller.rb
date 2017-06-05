a55c82b13af8b49a810e1e4be25bb58c69fef080
san4es <san4es113@gmail.com>
2017-06-05 17:23:05 +0300

    redis

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -45,7 +45,7 @@ class StatesController < ApplicationController
           c.get(st.gear) do |topic, message|
           
            
-                  st.state = st.property
+                  st.state = message.to_s.split(':')[0]
                   st.save
             
           end

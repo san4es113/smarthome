42e7dfcd562099984d3061dfad7a35737fc65a15
san4es <san4es113@gmail.com>
2017-06-05 17:28:47 +0300

    redis

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -44,10 +44,10 @@ class StatesController < ApplicationController
         MQTT::Client.connect(conn_opts) do |c|
           c.get(st.gear) do |topic, message|
           
-           
+           if st.property==message.to_s.split(':')[0]
                   st.state = message.to_s.split(':')[1]
                   st.save
-            
+            end
           end
           end
         end

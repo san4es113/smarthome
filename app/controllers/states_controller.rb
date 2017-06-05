b9c552425756eefa250cb85df0bf3d851300a707
san4es <san4es113@gmail.com>
2017-06-05 16:24:43 +0300

    redis

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -36,6 +36,34 @@ class StatesController < ApplicationController
           end
         end
 
+
+
+
+  @states.each do |st|
+      Thread.new do
+        MQTT::Client.connect(conn_opts) do |c|
+          # The block will be called when you messages arrive to the topic
+          c.get(st.gear) do |topic, message|
+                #if message.to_s.split(':')[0] == st.property
+                  st.state = message.to_s.split(':')[1]
+                  st.save
+            #end
+          end
+          end
+        end
+      end
+
+      end
+
+
+
+
+
+
+
+
+        
+
   end  
  
 

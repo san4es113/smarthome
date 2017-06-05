78992d43c5265e83ddc30d69a5ba7516a2e16375
san4es <san4es113@gmail.com>
2017-06-05 16:23:15 +0300

    redis

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -23,10 +23,10 @@ class ReportWorker
         MQTT::Client.connect(conn_opts) do |c|
           # The block will be called when you messages arrive to the topic
           c.get(st.gear) do |topic, message|
-                if message.to_s.split(':')[0] == st.property
+                #if message.to_s.split(':')[0] == st.property
                   st.state = message.to_s.split(':')[1]
                   st.save
-            end
+            #end
           end
           end
         end

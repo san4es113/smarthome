f59df751dedf5860b47a5d980e16a247f96b18dd
san4es <san4es113@gmail.com>
2017-06-06 08:23:49 +0300

    redis

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -33,7 +33,7 @@ class StatesController < ApplicationController
       Thread.new do
         MQTT::Client.connect(conn_opts) do |c|
           c.get(st.gear) do |topic, message|
-                  st.state = st.property
+                  st.state = message
                   st.save
 
           end

fb0349fa9e074a1b0b058f4a210a7cda04b1c735
san4es <san4es113@gmail.com>
2017-06-06 13:03:12 +0300

    s

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,7 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'san4es113@gmail.com'
+ 
   def contact_email(name, email, body)
-    @name = name
-    @email = email
-    @body = "Розпізнано нову особу , щоб переглянути відвідайте сайт розумного дому"
+    
     mail(from:"san4es113@gmail.com",to: "san4es113@gmail.com"  , subject: 'The message from support')
   end
 end
